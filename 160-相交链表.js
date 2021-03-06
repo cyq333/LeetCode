8288a0596f4618fff7e7692a62ca7f43269e22dd
yiqing.chen <yiqing.chen@weimob.com>
2021-03-06 16:01:40 +0800

    160

---
+++ 160-相交链表.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+//双指针法
+var getIntersectionNode = function (headA, headB) {
+  if (!headA || !headB) return null;
+  let pA = headA;
+  let pB = headB;
+  while (pA !== pB) {
+    pA = pA === null ? headB : pA.next;
+    pB = pB === null ? headA : pB.next;
+  }
+  return pA;
+};
