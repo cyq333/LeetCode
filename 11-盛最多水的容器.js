ad19984cb0ef36cfb0f44aadf1953d2e66feea56
yiqing.chen <yiqing.chen@weimob.com>
2021-03-14 21:08:03 +0800

    11

---
+++ 11-盛最多水的容器.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let i = 0;
+  let j = height.length - 1;
+  let square = 0;
+  let max = 0;
+  while (i < j) {
+    if (height[i] > height[j]) {
+      square = height[j] * (j - i);
+      j--;
+    } else {
+      square = height[i] * (j - i);
+      i++;
+    }
+    max = Math.max(square, max);
+  }
+  return max;
+};
