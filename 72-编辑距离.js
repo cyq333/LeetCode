23e8aaaa22bf5553ea80bf474ff4ed19cea0ceaa
yiqing.chen <yiqing.chen@weimob.com>
2021-03-25 14:01:34 +0800

    72

---
+++ 72-编辑距离.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+//动态规划 O(mn) O(mn)
+var minDistance = function (word1, word2) {
+  let n = word1.length;
+  let m = word2.length;
+  let dp = new Array(n + 1).fill(0).map(() => new Array(m + 1).fill(0));
+  for (let i = 0; i <= n; i++) {
+    dp[i][0] = i;
+  }
+  for (let j = 0; j <= m; j++) {
+    dp[0][j] = j;
+  }
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= m; j++) {
+      if (i * j) {
+        dp[i][j] =
+          word1[i - 1] === word2[j - 1]
+            ? dp[i - 1][j - 1]
+            : Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])+1;
+      } else {
+        dp[i][j] = i + j;
+      }
+    }
+  }
+  return dp[n][m];
+};
