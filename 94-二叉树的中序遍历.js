995af8eb67708358ad07dd3cc916e0c158e58f17
yiqing.chen <yiqing.chen@weimob.com>
2021-03-28 15:31:55 +0800

    94

---
+++ 94-二叉树的中序遍历.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+//递归 O(n) O(n)
+var inorderTraversal = function (root) {
+  let res = [];
+  const inorder = (root) => {
+    if (!root) {
+      return;
+    }
+    inorder(root.left);
+    res.push(root.val);
+    inorder(root.right);
+  };
+  inorder(root);
+  return res;
+};
+
+//迭代 O(n) O(n)
+var inorderTraversal = function (root) {
+  let res = [];
+  let stk = [];
+  while (root || stk.length) {
+    while (root) {
+      stk.push(root);
+      root = root.left;
+    }
+    root = stk.pop();
+    res.push(root.val);
+    root = root.right;
+  }
+  return res;
+};
